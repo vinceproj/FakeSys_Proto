f18b89c8616a02c2b64b0785fdfcbbeeaeeffcfc
vinceproj <vince.project.design@gmail.com>
2019-10-25 18:36:20 -0700

    Add local notes functionality
    Working on local encryption

--- script/secondary/notesSys.js
+++ script/secondary/notesSys.js
@@ -1,5 +1,16 @@
 const {ipcRenderer} = require("electron");
 const $ = require('jquery');
+const fs = require('fs');
+const crypto = require('crypto')
+
+const filename = 'localSchedule'
+
+const algorithm = 'aes-192-cbc';
+const password = 'for key generation here'
+const key = crypto.scryptSync(password, 'salt', 24);
+const iv = 'aaaabbbbccccdddd'
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+
 
 $('#homePageButton').on('click', ()=>{
     ipcRenderer.send('mainPage-GO',()=>{});
@@ -12,3 +23,69 @@ $('#signOutButton').on('click', ()=>{
 $('#exitButton').on('click', ()=>{
     ipcRenderer.send('exit-GO', ()=>{});
 })
+
+//Date functionality
+let currentDate;
+let dateFinder = new Date();
+currentDate = (dateFinder.getMonth()+1) + "/" + dateFinder.getDate() + "/" + dateFinder.getFullYear()
+console.log(currentDate);
+
+$("#showCurrentDate").text("Today is " + currentDate)
+
+//Encryption functionality
+function encrypt(stringTo){
+    cipher.on('readable', ()=>{
+        let chunk;
+        while(null!== (chunk = cipher.read())){
+            stringTo += chunk.toString('hex');
+            stringTo += '\n'
+        }
+    })
+
+    cipher.end();
+    console.log(stringTo)
+
+
+}
+
+//Task functionality
+$("#addTaskButton").on('click', ()=>{
+   let task = $('#taskString').val();
+
+    addEntry(task);
+    $('#taskString').val("");
+
+
+    encrypt(task);
+
+    fs.appendFile(filename, task +'\n', function(err){
+        if (err) throw err;
+    })
+})
+
+function addEntry(task){
+    let updateString = '<tr><td>' + currentDate + '</td><td></td><td>' + task + '</td>';
+    $('#taskTable').append(updateString);
+}
+
+function loadSchedule(){
+    //Check if file exists
+    if(fs.existsSync(filename)){
+        let data = fs.readFileSync(filename, 'utf8').split('\n');
+
+        data.forEach((readEncrypted, index)=>{
+            //let readText = decrypt(readEncrypted);
+            if(readEncrypted) 
+            addEntry(readEncrypted);
+
+        })
+    }else {
+        console.log("error");
+        fs.writeFile(filename, "", (err)=>{
+            if(err)
+            console.log(err)
+        })
+    }
+}
+
+loadSchedule();
